6e74d908a577b9a56d92987e4dc348201d639b21
Le Van Thai <lethai281005@gmail.com>
2026-09-15 23:41:27 +0700

    [CHORE] (server): Config database, http logger, client cookie

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import express, { type Express, type Request, type Response } from "express";
+import "dotenv/config";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import connect from "./config/connect.ts";
 
 const app: Express = express();
+
+//Connect to Database
+const connectDB = async () => {
+  await connect();
+};
+connectDB();
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
