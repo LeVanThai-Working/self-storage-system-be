c98113235d43eeea6b7f13559e08cfa27972b0d4
Le Van Thai <lethai281005@gmail.com>
2026-09-19 00:35:41 +0700

    [FEATURE] (user): User module

---
+++ src/modules/auth/oauth2.auth.ts
@@ -0,0 +1,37 @@
+// import passport from 'passport';
+// import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+// import { type Request } from 'express';
+
+// // const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || 'YOUR_CLIENT_ID';
+// // const GOOGLE_CLIENT_SECRET =
+// //   process.env.GOOGLE_CLIENT_SECRET || 'YOUR_CLIENT_SECRET';
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID:
+//         '',
+//       clientSecret: '',
+//       callbackURL: 'http://localhost:5000/google/callback',
+//       passReqToCallback: true,
+//     },
+//     // Định nghĩa kiểu tường minh cho các tham số trong callback function
+//     function (
+//       request: Request,
+//       accessToken: string,
+//       refreshToken: string,
+//       profile: any,
+//       done: any => void
+//     ): void {
+//       return done(err, profile);
+//     }
+//   )
+// );
+
+// passport.serializeUser(function(user, done) {
+//     done(null, user)
+// })
+
+// passport.deserializeUser(function(user: any, done) {
+//     done(null, user)
+// })
